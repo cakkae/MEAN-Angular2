6857b917783ec2e37812bed75e49810d0b391585
Adnan Beganović <cakkae@hotmail.com>
2018-06-07 07:43:45 +0200

    Deploying...

--- routes/authentication.js
+++ routes/authentication.js
@@ -159,6 +159,25 @@ module.exports = (router) => {
   /* ================================================
   MIDDLEWARE - Used to grab user's token from headers
   ================================================ */
+  router.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+  
   router.use((req, res, next) => {
     const token = req.headers['authorization']; // Create token found in headers
     // Check if token was found in headers
