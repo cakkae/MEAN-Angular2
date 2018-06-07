ebe72ea0a8cb95b433968e2ee1923d73bcdfed4e
Adnan Beganović <cakkae@hotmail.com>
2018-06-07 07:57:24 +0200

    Deploy.

--- index.js
+++ index.js
@@ -27,21 +27,22 @@ mongoose.connect(config.uri, (err) => {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    app.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
 
     // Request methods you wish to allow
-    app.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    app.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    app.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
 });
+app.use(cors({origin: 'http://localhost:8080'}));
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
