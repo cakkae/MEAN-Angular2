d6283f968c69555714e7c58af9172a9c61690f13
Adnan Beganović <cakkae@hotmail.com>
2018-06-07 07:49:44 +0200

    dep

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ mongoose.connect(config.uri, (err) => {
 });
 
 // Middleware
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    app.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    app.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    app.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    app.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
